f8067b8b308e529bb4631a582321fff316978da9
Yongho Kim <kyhcraft@gmail.com>
2025-01-08 14:27:17 +0900

    ♻️  refactor: Gatsby Head API 적용
    styled-components의 createGlobalStyle에서 @import를 권장하지 않고 head 태그 내에 link태그를 통해서 불러오는것을 권장

--- src/components/common/Layout.tsx
+++ src/components/common/Layout.tsx
@@ -6,7 +6,6 @@ type LayoutProps = {
 }
 
 const GlobalStyle = createGlobalStyle`
-  @import url('<https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.6/dist/web/static/pretendard.css>');
 
   * {
     margin: 0;
