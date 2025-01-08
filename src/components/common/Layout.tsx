449e2bc82979b846ba19de25a273586b415f6411
Yongho Kim <kyhcraft@gmail.com>
2025-01-08 14:00:40 +0900

    💄  design: 전역 스타일 설정

--- src/components/common/Layout.tsx
+++ src/components/common/Layout.tsx
@@ -1,3 +1,47 @@
-export const Layout = () => {
-  return <div>Layout</div>
+import { ReactNode } from 'react'
+import styled, { createGlobalStyle } from 'styled-components'
+
+type LayoutProps = {
+  children: ReactNode
+}
+
+const GlobalStyle = createGlobalStyle`
+  @import url('<https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.6/dist/web/static/pretendard.css>');
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Pretendard';
+  }
+
+  html,
+  body,
+  #___gatsby,
+  #gatsby-focus-wrapper {
+    min-height: 100%;
+    height: 100%;
+  }
+`
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 1000px;
+  min-height: 100%;
+  margin: 0 auto;
+`
+
+const Contents = styled.div`
+  margin: 80px 0;
+`
+
+export default function Layout({ children }: LayoutProps) {
+  return (
+    <Wrapper>
+      <GlobalStyle />
+
+      <Contents>{children}</Contents>
+    </Wrapper>
+  )
 }
