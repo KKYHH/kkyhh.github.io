a6c7adc66c94031c605392545a2fb64af1a9f8e2
Yongho Kim <kyhcraft@gmail.com>
2025-01-14 16:23:36 +0900

    ✨  feat: 카테고리 설정

--- src/components/main/Category.tsx
+++ src/components/main/Category.tsx
@@ -1,3 +1,40 @@
-export const Category = () => {
-  return <div>Category</div>
+import styled from 'styled-components'
+
+type CategoryProps = {
+  categories: Record<string, number>
+  selectedCategory: string
+  handleSelect: (category: string) => void
+}
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 5px 20px;
+  margin-top: 80px;
+`
+
+const Item = styled.div<{ $selected: boolean }>`
+  font-size: 16px;
+  font-weight: ${({ $selected }) => ($selected ? 600 : 300)};
+  cursor: ${({ $selected }) => ($selected ? 'default' : 'pointer')};
+`
+
+export default function Category({
+  categories,
+  selectedCategory,
+  handleSelect,
+}: CategoryProps) {
+  return (
+    <Wrapper>
+      {Object.entries(categories).map(([category, count]) => (
+        <Item
+          onClick={() => handleSelect(category)}
+          $selected={category === selectedCategory}
+          key={category}
+        >
+          #{category}({count})
+        </Item>
+      ))}
+    </Wrapper>
+  )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,39 @@
+import { useState } from 'react'
 import { graphql, PageProps } from 'gatsby'
 import Introduction from '../components/main/Introduction'
+import Category from '../components/main/Category'
 
 export default function Index({
   data: {
     allContentfulBlog: { nodes },
   },
 }: PageProps<Queries.IndexPageQuery>) {
+  const [selectedCategory, setSelectedCategory] = useState<string>('All')
+
+  const categories = nodes.reduce<Record<string, number>>(
+    (categories, post) => {
+      post.category
+        ?.filter((category): category is string => !!category)
+        .forEach(
+          category => (categories[category] = (categories[category] ?? 0) + 1),
+        )
+
+      return categories
+    },
+    { All: nodes.length },
+  )
+
+  const handleSelectCategory = (category: string) =>
+    setSelectedCategory(category)
+
   return (
     <>
       <Introduction />
+      <Category
+        categories={categories}
+        selectedCategory={selectedCategory}
+        handleSelect={handleSelectCategory}
+      />
 
       {nodes.map(({ title, slug, date }) => (
         <div key={slug}>
@@ -24,6 +49,7 @@ export const query = graphql`
     allContentfulBlog(sort: { date: DESC }) {
       nodes {
         title
+        category
         slug
         date
       }
