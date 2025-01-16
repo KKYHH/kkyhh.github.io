700e6cd692721309f23f885b1f418231f58866c0
Yongho Kim <kyhcraft@gmail.com>
2025-01-16 15:55:29 +0900

    ✨  feat: 게시글 헤드 바디 추가

--- src/components/post/PostBody.tsx
+++ src/components/post/PostBody.tsx
@@ -1,3 +1,37 @@
-export const PostBody = () => {
-  return <div>PostBody</div>
+import styled from 'styled-components'
+
+type PostBodyProps = {
+  content: Queries.ContentfulBlog
+}
+
+const Wrapper = styled.div`
+  position: relative;
+  display: grid;
+  grid-template-columns: 1fr 220px;
+  grid-gap: 30px;
+  justify-content: space-between;
+  align-items: flex-start;
+  padding-top: 100px;
+`
+
+const Content = styled.div`
+  overflow: auto;
+  display: flex;
+  flex-direction: column;
+  gap: 100px;
+  font-size: 16px;
+  line-height: 2;
+  word-break: break-word;
+`
+
+export default function PostBody({ content }: PostBodyProps) {
+  return (
+    <Wrapper>
+      <Content>
+        <div id="content">{/* 렌더링된 게시글이 들어갈 자리 */}</div>
+        {/* 댓글 컴포넌트가 들어갈 자리 */}
+      </Content>
+      {/* 플로팅 목차 컴포넌트가 들어갈 자리 */}
+    </Wrapper>
+  )
 }
---
+++ src/pages/{contentfulBlog.slug}.tsx
@@ -0,0 +1,36 @@
+import { PageProps, graphql } from 'gatsby'
+import { IGatsbyImageData } from 'gatsby-plugin-image'
+import PostHead from '../components/post/PostHead'
+
+export default function Post({
+  data: { contentfulBlog },
+}: PageProps<Queries.PostPageQuery>) {
+  return (
+    <>
+      <PostHead
+        title={contentfulBlog?.title as string}
+        category={contentfulBlog?.category as string[]}
+        date={contentfulBlog?.date as string}
+        thumbnail={
+          contentfulBlog?.thumbnail?.gatsbyImageData as IGatsbyImageData
+        }
+      />
+    </>
+  )
+}
+
+export const query = graphql`
+  query PostPage($slug: String!) {
+    contentfulBlog(slug: { eq: $slug }) {
+      title
+      thumbnail {
+        gatsbyImageData(width: 1000)
+      }
+      category
+      date
+      content {
+        raw
+      }
+    }
+  }
+`
